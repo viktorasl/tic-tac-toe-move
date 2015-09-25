34e13d27e966a3f2121b68a4c9464edc21523f5c
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-09-25 22:25:07 +0300

    Pattern matching for first symbol in a given string

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -1,5 +1,6 @@
 module TicTacToe
 where
+import Data.Char
 
 type BoardField = (Int, Int, Char)
 type Board = [BoardField]
@@ -11,7 +12,9 @@ move :: String -> Maybe BoardField
 move boardBencode = Just $ putXInEmptyField $ parseBoard boardBencode
 
 parseBoard :: String -> Board
-parseBoard _ = []
+parseBoard ('d' : dict) = [(0, 0, 'd')]
+parseBoard ('l' : list) = [(0, 0, 'l')]
+parseBoard (l : string) = if (isNumber l) then [(0, 0, 's')] else []
 
 putXInEmptyField :: Board -> BoardField
-putXInEmptyField _ = (0, 0, 'x')
+putXInEmptyField board = head board
