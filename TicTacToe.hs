aeea2c5635d71bfda7b3aa3baf4d27081ce24309
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-09-29 12:40:53 +0300

    Parsing the board

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -44,22 +44,40 @@ message :: String
 
 message = "ld1:v1:x1:xi1e1:yi1eed1:v1:o1:xi0e1:yi2eed1:v1:x1:xi2e1:yi2eed1:v1:o1:xi0e1:yi1eee"
 
-move :: String -> Maybe BoardField
-move boardBencode = Just $ putXInEmptyField $ parseBoard boardBencode
+--move :: String -> Maybe BoardField
+--move boardBencode = Just $ putXInEmptyField $ parseBoard boardBencode
 
 parseBoard :: String -> Board
-parseBoard ('l' : list) = parseBoardFields list
-parseBoard _ = []
+parseBoard ('l' : list) = parseBoardFields' list []
 
-parseBoardFields :: String -> Board
-parseBoardFields ('d' : dict) = 
+parseBoardFields' :: String -> Board -> Board
+parseBoardFields' ('e' : dict) board = board
+parseBoardFields' ('d' : dict) board =
     let
-    (field, valueRest) = parseBoardField $ drop 2 dict
-    in [field]
-parseBoardFields _ = []
+    (field, rest) = parseBoardField' dict (0, 0, 'x')
+    in parseBoardFields' rest (field : board)
 
-parseBoardField :: String -> (BoardField, String)
-parseBoardField (key : value) = ((1, 1, key), value)
+parseChar :: String -> Char
+parseChar str = head $ drop 2 str
 
-putXInEmptyField :: Board -> BoardField
-putXInEmptyField board = head board
+parseInt :: String -> Int
+parseInt str = digitToInt $ head $ drop 1 str
+
+assignValueToKey :: String -> Char -> BoardField -> (BoardField, String)
+assignValueToKey str 'v' (x, y, v) = ((x, y, parseChar str), drop 3 str)
+assignValueToKey str 'x' (x, y, v) = ((parseInt str, y, v), drop 3 str)
+assignValueToKey str 'y' (x, y, v) = ((x, parseInt str, v), drop 3 str)
+
+parseBoardField' :: String -> BoardField -> (BoardField, String)
+parseBoardField' ('e' : rest) field = (field, rest) 
+parseBoardField' str field =
+    let
+    (key, rest) = parseBoardFieldKey str
+    (field', rest') = assignValueToKey rest key field
+    in parseBoardField' rest' field'
+
+parseBoardFieldKey :: String -> (Char, String)
+parseBoardFieldKey str = (head $ drop 2 str, drop 3 str)
+
+--putXInEmptyField :: Board -> BoardField
+--putXInEmptyField board = head board
